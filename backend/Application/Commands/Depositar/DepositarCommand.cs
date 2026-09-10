44f45df60776cfc4c129196b983f7b73850fd484
Leila Peso <leilapeso005@gmail.com>
2026-09-09 21:30:58 -0300

    terminamos endpoints

---
+++ backend/Application/Commands/Depositar/DepositarCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Commands.Depositar;
+
+public class DepositarCommand
+{
+    public int UsuarioId { get; set; }
+    public decimal Monto { get; set; }
+
+    public DepositarCommand(int usuarioId, decimal monto)
+    {
+        UsuarioId = usuarioId;
+        Monto = monto;
+    }
+}
