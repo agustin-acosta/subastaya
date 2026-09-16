5b93a37b9f86eeec0f59935479e0b72aa57d1679
Leila Peso <leilapeso005@gmail.com>
2026-09-16 17:46:49 -0300

    problema con la finalizacion de subastas

--- backend/Infrastructure/SubastaYaDbContext.cs
+++ backend/Infrastructure/SubastaYaDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure;
 
@@ -21,6 +22,30 @@ public class SubastaYaDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(SubastaYaDbContext).Assembly);
+
+        var conversorUtc = new ValueConverter<DateTime, DateTime>(
+            aGuardar => aGuardar,
+            aLeer => DateTime.SpecifyKind(aLeer, DateTimeKind.Utc));
+
+        var conversorUtcNulable = new ValueConverter<DateTime?, DateTime?>(
+            aGuardar => aGuardar,
+            aLeer => aLeer.HasValue ? DateTime.SpecifyKind(aLeer.Value, DateTimeKind.Utc) : aLeer);
+
+        foreach (var entidad in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var propiedad in entidad.GetProperties())
+            {
+                if (propiedad.ClrType == typeof(DateTime))
+                {
+                    propiedad.SetValueConverter(conversorUtc);
+                }
+                else if (propiedad.ClrType == typeof(DateTime?))
+                {
+                    propiedad.SetValueConverter(conversorUtcNulable);
+                }
+            }
+        }
+
         base.OnModelCreating(modelBuilder);
     }
 }
